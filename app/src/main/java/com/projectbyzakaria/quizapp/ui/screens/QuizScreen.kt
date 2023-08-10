5e07898883a18fabc3d3bdfbca5a0b7455b4ebb6
zakaria <zakareyahamidou3@gmail.com>
2023-08-10 19:11:03 +0200

    add quiz Screen

---
+++ app/src/main/java/com/projectbyzakaria/quizapp/ui/screens/QuizScreen.kt
@@ -0,0 +1,11 @@
+package com.projectbyzakaria.quizapp.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun QuizScreen(
+    modifier :Modifier = Modifier
+) {
+
+}
